3dd4493faea374e57559634cdeaaaa016e3ce676
Okan Sahin <okan.shn@hotmail.com>
2022-02-01 13:28:09 +0300

    all

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_ForEach_Döngüleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ortalaması alınacak sayıyı sonunu giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac=1;
+            int toplam=0;
+
+            while (sayac<=sayi)
+            {
+                toplam=toplam+sayac;
+                sayac++;
+            }
+            Console.WriteLine("Ortalama: "+toplam/sayi);
+        }
+    }
+}
